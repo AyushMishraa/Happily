977b632e09a44c24c8c268971f984bddd342790f
AyushMishraa <educationsoftware116@gmail.com>
2023-09-20 23:43:23 +0530

    changes done

--- react-web-page/src/components/Hero/Hero.jsx
+++ react-web-page/src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export const Hero=()=>{
     return (
     <section className={styles.container}>
         <div className={styles.content}>
-          <h1 className={styles.title}>Hi, Let's Be Happy !</h1>
+          <h1 className={styles.title}>Hi, Let's Be Happy!</h1>
           <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, voluptatum rerum explicabo esse praesentium sequi eos ipsa illo adipisci consectetur itaque, officia, soluta exercitationem reiciendis suscipit corrupti minus quis voluptas!</p>
         </div>
         <img src={getImageUrl("hero/heroImage.png")} alt="Hero Image" className={styles.heroImg}/>
